091047e1d72a5cb60588106304bd4220013e48a1
teamironowl <teamironowl@gmail.com>
2020-06-19 21:58:37 +0630

    first commit

---
+++ build/precache-manifest.4d608e5024828995ce27edb8c3d74f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b857e496bd20f56e7d8474a09d97d80c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9525a5b15789f84040bd",
+    "url": "/static/css/main.c7446496.chunk.css"
+  },
+  {
+    "revision": "70eb2ba58c1f14d2991c",
+    "url": "/static/js/2.0cdcccab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0cdcccab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9525a5b15789f84040bd",
+    "url": "/static/js/main.752c1d39.chunk.js"
+  },
+  {
+    "revision": "c644f31583d32803adf0",
+    "url": "/static/js/runtime-main.5252ea5e.js"
+  }
+]);
